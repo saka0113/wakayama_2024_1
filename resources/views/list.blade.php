56c56d4bfd95e167944dfe5c4aae223fd66ac4d8
Midu369 <156885131+Midu369@users.noreply.github.com>
2024-08-21 17:13:52 +0900

    update list.blade.php & add list.css

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,13 +1,24 @@
 <x-app-layout>
   @push('styles')
   <link rel="stylesheet" href="{{ asset('css/create.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/top.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/list.css') }}">
   @endpush
+    <header class="title">
+        <div class="title-child"></div>
+        <a class="a">和歌山うちわ飯</a>
+    </header>
 
   <div>
     <h1>paiza bbs</h1>
     <p>{{ $message }}</p>
-    @foreach ($articles as $article)
-      <p>{{ $article->content }}</p>
-    @endforeach
+    <div class="square_contents">
+        @foreach ($articles as $article)
+            <div class="square_content">
+                <a href="./{{$article->content}}"><img class="square_image" src="{{ $article->content }}"></a>
+                <p>test</p>
+            </div>
+        @endforeach
+    </div>
   </div>
 </x-app-layout>
