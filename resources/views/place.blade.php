274a409aeaddd3145ad5de62b606f785f26aba48
Midu369 <156885131+Midu369@users.noreply.github.com>
2024-08-22 17:54:06 +0900

    選択肢の変更とボタンの微修正

--- resources/views/place.blade.php
+++ resources/views/place.blade.php
@@ -20,6 +20,7 @@
                     <a href="" class="placename">かつらぎ町</a>
                     <a href="" class="placename">九度山町</a>
                     <a href="" class="placename">高野町</a>
+                    <a href="" class="placename">紀美野町</a>
                 </div>
             </div>
 
@@ -27,12 +28,21 @@
             <div class="place-section">
                 <div class="place-title">紀中</div>
                 <div class="place-items">
+                    <a href="" class="placename">有田市</a>
                     <a href="" class="placename">御坊市</a>
                     <a href="" class="placename">田辺市</a>
-                    <a href="" class="placename">みなべ町</a>
-                    <a href="" class="placename">日高町</a>
-                    <a href="" class="placename">印南町</a>
+                    <a href="" class="placename">有田川町</a>
+                    <a href="" class="placename">湯浅町</a>
+                    <a href="" class="placename">広川町</a>
                     <a href="" class="placename">由良町</a>
+                    <a href="" class="placename">日高町</a>
+                    <a href="" class="placename">美浜町</a>
+                    <a href="" class="placename">日高川町</a>
+                    <a href="" class="placename">印南町</a>
+                    <a href="" class="placename">みなべ町</a>
+                    <a href="" class="placename">上富田町</a>
+                    <a href="" class="placename">白浜町</a>
+                    <a href="" class="placename">すさみ町</a>
                 </div>
             </div>
 
@@ -40,14 +50,12 @@
             <div class="place-section">
                 <div class="place-title">紀南</div>
                 <div class="place-items">
-                    <a href="" class="placename">南紀白浜町</a>
-                    <a href="" class="placename">上富田町</a>
-                    <a href="" class="placename">すさみ町</a>
+                    <a href="" class="placename">新宮市</a>
+                    <a href="" class="placename">古座川町</a>
                     <a href="" class="placename">串本町</a>
                     <a href="" class="placename">那智勝浦町</a>
-                    <a href="" class="placename">本宮町</a>
-                    <a href="" class="placename">熊野市</a>
-                    <a href="" class="placename">新宮市</a>
+                    <a href="" class="placename">太地町</a>
+                    <a href="" class="placename">北山町</a>
                 </div>
             </div>
 
