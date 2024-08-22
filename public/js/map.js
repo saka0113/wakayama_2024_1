f16a60bc6c65ea9c6a251e995b00c73ee9f5b1aa
saka0113 <satoshi_800@iclud.com>
2024-08-22 16:29:21 +0900

    コピー送信

---
+++ public/js/map.js
@@ -0,0 +1,41 @@
+function initMap() {
+    const wakayamaBounds = {
+        north: 34.5100,
+        south: 33.40000,
+        east: 136.3000,
+        west: 134.9000
+    };
+const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 34, lng: 135.45 }, // 東京を中心に設定
+    zoom: 8.5,
+    restriction: {
+        latLngBounds: wakayamaBounds,
+        strictBounds: true, // この設定で制限エリア外の移動を防ぐ
+    },
+    
+});
+
+map.addListener("click", (event) => {
+    getAddress(event.latLng);
+});
+}
+
+function getAddress(latLng) {
+const geocoder = new google.maps.Geocoder();
+geocoder.geocode({ location: latLng }, (results, status) => {
+    if (status === "OK" && results[0]) {
+        const cityInfo = results[0].address_components.find(component => component.types.includes("locality"));
+        const municipality = cityInfo ? cityInfo.long_name : "市町村情報が見つかりませんでした。";
+        document.getElementById("location-info").innerText = "市町村: " + municipality;
+        if (municipality) {
+            // 市町村名をURLのクエリパラメータとして追加
+            const newUrl = new URL(window.location.href);
+            newUrl.searchParams.set('municipality', municipality);
+            window.history.pushState({}, '', newUrl);
+        }
+    } else {
+        document.getElementById("location-info").innerText = "位置情報の取得に失敗しました。";
+    }
+});
+}
+
