dd23f83cb415b8946834dd37e9f5c37a74521753
西村はるほ <s276183@wakayama-u.ac.jp>
2024-08-23 10:31:06 +0900

    リスト画面の調整

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -6,20 +6,20 @@
   <x-slot name="header"></x-slot>
 
   <div>
+  <h1>{{$message}}</h1>
     <div class="square_contents">
-      <h1>{{$message}}</h1>
       @foreach ($articles as $article)
-      <div class="square_content">
-        <a href='{{ route("article.show", ["id" => $article->id]) }}'>
-          <img class="square_image" src="{{ asset('storage/' . $article->image_path) }}" alt="Article Image">
-        </a>
-        <div class="icon_box ">
-          <img class="icon_size" alt="" src="{{ asset('images/icon.svg') }}">
-          <!-- クリック時に'いいね'のカウントが増えるようにする -->
-          <img class="icon_size" alt="" src="{{ asset('images/icon-11.svg') }}">
-          <!-- キープも同様 -->
+        <div class="square_content">
+          <a href='{{ route("article.show", ["id" => $article->id]) }}'>
+            <img class="square_image" src="{{ asset('storage/' . $article->image_path) }}" alt="Article Image">
+          </a>
+          <div class="icon_box ">
+            <img class="icon_size" alt="" src="{{ asset('images/icon.svg') }}">
+            <!-- クリック時に'いいね'のカウントが増えるようにする -->
+            <img class="icon_size" alt="" src="{{ asset('images/icon-11.svg') }}">
+            <!-- キープも同様 -->
+          </div>
         </div>
-      </div>
       @endforeach
     </div>
   </div>
