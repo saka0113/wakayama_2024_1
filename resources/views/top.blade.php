e3c8edb91cb3ca88b9f1a031bb5df1d4efaae537
jupeeze <pokegames1578@gmail.com>
2024-08-27 13:12:34 +0900

    Merge remote-tracking branch 'origin' into 0007-list-favorite

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     @push('styles')
-        <link rel="stylesheet" href="{{ asset('css/top.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/top.css') }}" />
     @endpush
     <!-- <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -26,13 +26,13 @@
                 <div class="div">投稿</div>
             </div>
         </section>
-        <section class="content1">
-            <div class="mypage" id="mypageContainer">
-                <div class="profile-picture"></div>
-                <img class="profile-icon" loading="lazy" alt="" src="{{ asset('images/vector.svg') }}" />
-                <div class="my-page-label">
-                    <div class="div1">マイページ</div>
+        <section class="content">
+            <div class="toukou" id="kensakuContainer">
+                <div class="toukou-child"></div>
+                <div class="toukou-wrapper">
+                    <img class="toukou-icon" alt="" src="{{ asset('images/kensaku.svg') }}" />
                 </div>
+                <div class="div">検索</div>
             </div>
         </section>
     </div>
@@ -45,19 +45,26 @@
     </div>
 
     <script>
-        document.addEventListener('DOMContentLoaded', function () {
+        document.addEventListener('DOMContentLoaded', function() {
+            var googlemapImage = document.getElementById("googlemapImage");
+            if (googlemapImage) {
+                googlemapImage.addEventListener("click", function(e) {
+                    window.location.href = "{{ url('list') }}";
+                });
+            }
+            
             // 投稿ボタンをクリックしたときの処理
             var toukouContainer = document.getElementById("toukouContainer");
             if (toukouContainer) {
-                toukouContainer.addEventListener("click", function (e) {
+                toukouContainer.addEventListener("click", function(e) {
                     window.location.href = '{{ route("article.create") }}';
                 });
             }
 
             // 検索アイコンをクリックしたときの処理
-            var kensakuIcon = document.getElementById("kensaku");
+            var kensakuIcon = document.getElementById("kensakuContainer");
             if (kensakuIcon) {
-                kensakuIcon.addEventListener("click", function () {
+                kensakuIcon.addEventListener("click", function() {
                     var popup = document.getElementById("searchPopup");
                     popup.style.display = "flex";
                 });
